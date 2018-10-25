d43328040ff6bc7cd413f604abde007b2b6ee357
julianmca <julianmartinca@gmail.com>
2018-10-25 12:08:21 -0700

    Adjusted brackets.

--- TestProject/src/Hello.java
+++ TestProject/src/Hello.java
@@ -1,7 +1,9 @@
 
-public class Hello {
+public class Hello 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+		{
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
@@ -14,7 +16,6 @@ public class Hello {
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-
-}
+		}
 
 }
