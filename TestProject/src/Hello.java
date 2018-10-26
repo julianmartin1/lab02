0ae993a2a46d2d44a95e5487d3d72c82e00260c8
aparicie <aparicie@MARCUS.ecs.csus.edu>
2018-10-25 18:33:44 -0700

    Added name.

--- TestProject/src/Hello.java
+++ TestProject/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("219714652 - This line was added by Julian Martin.");
 		System.out.println("219758098 - This line was added by Tatiana Grama.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219350834 - This line was added by Ely Aparicio.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
