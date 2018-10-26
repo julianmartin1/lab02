aa7a78cf4584b67e44b82d35fc4383180f1b0bb3
Lorens Hadi <Lorens Hadi@DESKTOP-43OMUGS>
2018-10-25 20:19:46 -0700

    Added ID and name to System.out.println Lorens

--- TestProject/src/Hello.java
+++ TestProject/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("219714652 - This line was added by Julian Martin.");
 		System.out.println("219758098 - This line was added by Tatiana Grama.");
 		System.out.println("219350834 - This line was added by Ely Aparicio.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219943296 - This line was added by Lorens Hadi.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
